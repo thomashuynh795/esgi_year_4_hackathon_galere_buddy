efef700e0377f837eee7a6bc320b74dca2da291e
kani <gkounkoud@myges.fr>
2025-03-06 14:35:25 +0100

    Feat: Implement post creation, retrieval, updating, and deletion with tags

--- api/src/post/post.controller.ts
+++ api/src/post/post.controller.ts
@@ -1,7 +1,49 @@
-import { Controller } from "@nestjs/common";
-import { PostService } from "./post.service";
+import {Body, Controller, Delete, Get, Param, Patch, Post, Req, UseGuards} from "@nestjs/common";
+
+import {JwtGuard} from "../auth/guard/jwt.guard";
+import {CreatePostDto} from "./dto/create-post.dto";
+import {PostService} from "./post.service";
+import {CustomisedExpressRequest} from "../common/models/customised-express-request";
+import {UpdatePostDto} from "./dto/update-post.dto";
 
 @Controller("post")
 export class PostController {
-    constructor(private readonly postService: PostService) { }
+    constructor(private readonly postService: PostService) {}
+
+    @Get()
+    async findAll() {
+        return this.postService.findAll();
+    }
+
+    @Get(":id")
+    async findOne(@Param("id") id: string) {
+        return this.postService.findOne(id);
+    }
+
+    @Get("trending")
+    async findTrending() {
+        return this.postService.findTrending();
+    }
+
+    @Post()
+    @UseGuards(JwtGuard)
+    async create(@Body() createPostDto: CreatePostDto,
+                 @Req() req: CustomisedExpressRequest) {
+        return this.postService.create(createPostDto,req.user.id );
+    }
+
+    @Delete(":id")
+    @UseGuards(JwtGuard)
+    async delete(@Param("id") id: string,
+                 @Req() req: CustomisedExpressRequest) {
+        return this.postService.delete(id,req.user.id);
+    }
+
+    @Patch(":id")
+    @UseGuards(JwtGuard)
+    async update(@Param("id") id: string,
+                 @Body() updatePostDto: UpdatePostDto,
+                 @Req() req: CustomisedExpressRequest) {
+        return this.postService.update(id,updatePostDto,req.user.id);
+    }
 }
