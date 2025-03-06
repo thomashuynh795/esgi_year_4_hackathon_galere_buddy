c38c101b5a6c111c8cd51fe833bfef97961ccad3
yanislammari <yanis.lammari937@gmail.com>
2025-03-06 12:57:47 +0100

    add delete reaction request

--- api/src/reaction/reaction.controller.ts
+++ api/src/reaction/reaction.controller.ts
@@ -1,7 +1,33 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Delete, HttpStatus, Param, Res, UseGuards } from "@nestjs/common";
 import { ReactionService } from "./reaction.service";
+import { ReactionGuard } from "./guard/reaction.guard";
+import { Response } from "express";
+import { ErrorHandlerService } from "src/common/utils/error-handler/error-handler.service";
 
 @Controller("like")
 export class ReactionController {
-    constructor(private readonly reactionService: ReactionService) { }
+    constructor(
+        private readonly reactionService: ReactionService,
+        private readonly errorHandlerService: ErrorHandlerService
+    ) { }
+
+    @UseGuards(ReactionGuard)
+    @Delete(":id")
+    public async deleteReaction(
+        @Param("id") reactionId: string,
+        @Res() response: Response
+    ): Promise<Response> {
+        try {
+            await this.reactionService.deleteReaction(reactionId);
+
+            return response
+                .status(HttpStatus.NO_CONTENT);
+        } catch (error: any) {
+            return this.errorHandlerService
+            .getErrorForControllerLayer(
+                error,
+                response
+            );
+        }
+    }
 }
