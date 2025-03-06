5127dbcfe089328face840ffb285e22fc5d30ad6
Raphael GOISQUE <raph.goisque@gmail.com>
2025-03-06 16:21:17 +0100

    Add form to add a post

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,6 +1,19 @@
-"use client"
-import React from "react"
+// "use client"
+// import React from "react"
 
-export default function Dashboard() {
-    return <div>Dashboard page</div>
-}
+// export default function Dashboard() {
+//     return <div>Dashboard page</div>
+// }
+
+import React from 'react';
+import FormPublish from '@ui/organisms/FormPublish'; 
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to Home Page</h1>
+      <FormPublish />
+    </div>
+  );
+};
+
+export default HomePage;
